c5a1f000e99c342b61cb9c47851506e5c772e9b8
chulbulikushi07 <chulbulikushi07@gmail.com>
2026-08-05 22:59:16 +0530

    changing the links for the flask to function

--- app.py
+++ app.py
@@ -6,5 +6,37 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/assignments")
+def assignments():
+    return render_template("assignments.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/goals")
+def goals():
+    return render_template("goals.html")
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+@app.route("/pomodoro")
+def pomodoro():
+    return render_template("pomodoro.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/progress")
+def progress():
+    return render_template("progress.html")
+
+@app.route("/timetable")
+def timetable():
+    return render_template("timetable.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
