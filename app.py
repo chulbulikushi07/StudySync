62b344b3925c7c726d0fae8eb43b67746df0bec6
chulbulikushi07 <chulbulikushi07@gmail.com>
2026-08-24 00:23:22 +0530

    cleaned up the flask routes

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
+# importing tools from Flask
 from flask import Flask, render_template, request, redirect ,url_for
 
+# this creates the actual flask application
 app = Flask(__name__)
+
+#@app.route() means that, When someone visits /, execute the home() function , thi sis also called a decorator
+#URL -> Python function
+#render_template() is how flask talks to html, it looks inside templates/ finds the html file and sends it to the browser
+
+# -------------------- PUBLIC ROUTES --------------------
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/assignments", methods=["GET","POST"])
+# -------------------- ACADEMIC FEATURES --------------------
+
+#GET, used when you're asking something
+#POST, used when you're submiting data
+@app.route("/assignments", methods=["GET","POST"]) #This URL can handle both displaying the page and receiving submitted data.
 def assignments():
-    
+    #checks if the user input and data
     if request.method == "POST":
-        
+    
+    #connection between the frontend and the backend   
         title = request.form["title"]
         subject = request.form["subject"]
         due_date = request.form["due_date"]
         priority = request.form["priority"]
         description = request.form["description"]
-        
+    #prints out the information    
         print("Title:", title)
         print("Subject", subject)
         print("Due Date",due_date)
@@ -26,6 +39,9 @@ def assignments():
         return redirect(url_for("assignments"))
         
     return render_template("assignments.html")
+
+
+# -------------------- DASHBOARD --------------------
 
 @app.route("/dashboard")
 def dashboard():
@@ -39,14 +55,6 @@ def goals():
 def notes():
     return render_template("notes.html")
 
-@app.route("/pomodoro")
-def pomodoro():
-    return render_template("pomodoro.html")
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
 @app.route("/progress")
 def progress():
     return render_template("progress.html")
@@ -55,5 +63,25 @@ def progress():
 def timetable():
     return render_template("timetable.html")
 
-if __name__ == "__main__":
+# -------------------- PRODUCTIVITY --------------------
+
+@app.route("/pomodoro")
+def pomodoro():
+    return render_template("pomodoro.html")
+
+# -------------------- USER --------------------
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+# -------------------- RUN APPLICATION --------------------
+
+#If this Python file is being run directly, start the Flask server
+
+if __name__ == "__main__":   
     app.run(debug = True)
+       
+#is useful because Flask automatically reloads when you change your code.
+#It also gives you detailed error pages. 
