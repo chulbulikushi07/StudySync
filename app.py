08a16bb185ddeb447cd17be18ee181a4da8edbeb
chulbulikushi07 <chulbulikushi07@gmail.com>
2026-08-25 22:30:03 +0530

    Set up SQLite database and Assignment model

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 # importing tools from Flask
 from flask import Flask, render_template, request, redirect ,url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # this creates the actual flask application
 app = Flask(__name__)
 
-#@app.route() means that, When someone visits /, execute the home() function , thi sis also called a decorator
+# uses a SQLite database called studysync.db
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///studysync.db"
+#Disables modification tracking,it consumes additional resources and can generate unnecessary warnings.
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#connects SQLAlchemy to Flask,creates our database object
+db = SQLAlchemy(app)
+
+class Assignment(db.Model):
+    #nullable = False means the database cant accept an empty/null value.
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(150), nullable= False)
+    subject = db.Column(db.String(100), nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    priority = db.Column(db.String(20), nullable=False)
+    description = db.Column(db.Text)
+    completed = db.Column(db.Boolean, default=False)    
+
+#@app.route() means that, When someone visits /, execute the home() function , this is also called a decorator
 #URL -> Python function
 #render_template() is how flask talks to html, it looks inside templates/ finds the html file and sends it to the browser
 
@@ -80,7 +99,9 @@ def profile():
 
 #If this Python file is being run directly, start the Flask server
 
-if __name__ == "__main__":   
+if __name__ == "__main__": 
+    with app.app_context():
+        db.create_all()  
     app.run(debug = True)
        
 #is useful because Flask automatically reloads when you change your code.
